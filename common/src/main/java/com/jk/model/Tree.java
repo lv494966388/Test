33a03d391ec25920172a7ad706ae8d4900c0a1d3
lv494lv494 <lv494966388>
2022-07-25 17:22:07 +0800

    主分支改动

--- common/src/main/java/com/jk/model/Tree.java
+++ common/src/main/java/com/jk/model/Tree.java
@@ -1,33 +1,21 @@
 package com.jk.model;
 
-
-
 import java.io.Serializable;
 import java.util.List;
 
 
 public class Tree implements Serializable {
-
     private static final long serialVersionUID = 22L;
-
-
     private Integer id;
-
     private String text;
-
     private Integer pid;
-
     private String url;
-
-
     //异步树状态字段
     private String state;
-
     /**
      * 子节点集合
      */
     private List nodes;
-
 
     public static long getSerialVersionUID() {
         return serialVersionUID;
