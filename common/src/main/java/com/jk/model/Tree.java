5712864323294fd69006d0050e9d2b1d52170c7d
lv494lv494 <lv494966388>
2022-07-25 17:13:45 +0800

    主分支提交

--- common/src/main/java/com/jk/model/Tree.java
+++ common/src/main/java/com/jk/model/Tree.java
@@ -1,88 +1,82 @@
 package com.jk.model;
 
 
-
 import java.io.Serializable;
 import java.util.List;
 
 
+public class Tree implements Serializable {
+
+    private static final long serialVersionUID = 22L;
 
 
+    private Integer id;
 
-	public class Tree  implements Serializable{
+    private String text;
 
-		private static final long serialVersionUID =22L;
+    private Integer pid;
 
-
-
-		
-		 private   Integer    id;
-	     
-		 private   String     text;
-	     
-	     private   Integer    pid;
-	     
-	     private   String     url;
-	     
-
-		  	//异步树状态字段
-		  	private String state;
-		  	
-			/**
-			 * 子节点集合
-			 */
-		  	private  List   nodes;
+    private String url;
 
 
-		public static long getSerialVersionUID() {
-			return serialVersionUID;
-		}
+    //异步树状态字段
+    private String state;
 
-		public Integer getId() {
-			return id;
-		}
+    /**
+     * 子节点集合
+     */
+    private List nodes;
 
-		public void setId(Integer id) {
-			this.id = id;
-		}
 
-		public String getText() {
-			return text;
-		}
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
 
-		public void setText(String text) {
-			this.text = text;
-		}
+    public Integer getId() {
+        return id;
+    }
 
-		public Integer getPid() {
-			return pid;
-		}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-		public void setPid(Integer pid) {
-			this.pid = pid;
-		}
+    public String getText() {
+        return text;
+    }
 
-		public String getUrl() {
-			return url;
-		}
+    public void setText(String text) {
+        this.text = text;
+    }
 
-		public void setUrl(String url) {
-			this.url = url;
-		}
+    public Integer getPid() {
+        return pid;
+    }
 
-		public String getState() {
-			return state;
-		}
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
 
-		public void setState(String state) {
-			this.state = state;
-		}
+    public String getUrl() {
+        return url;
+    }
 
-		public List getNodes() {
-			return nodes;
-		}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-		public void setNodes(List nodes) {
-			this.nodes = nodes;
-		}
-	}
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List nodes) {
+        this.nodes = nodes;
+    }
+}
